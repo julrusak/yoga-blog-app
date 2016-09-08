ab419b268e275001c4dcbebb0ed3c275f53c0a05
julrusak <juliannarusak@gmail.com>
2016-09-08 18:22:41 -0400

    Add edit capability

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -15,14 +15,30 @@ end
 def create
 	@post = Post.new(post_params)
 	respond_to do |format|
-      if @post.save
-        format.html { redirect_to @post, notice: 'Post was successfully created.' }
-        format.json { render :show, status: :created, location: @post }
-      else
-        format.html { render :new }
-        format.json { render json: @post.errors, status: :unprocessable_entity }
-      end
+    if @post.save
+      format.html { redirect_to @post, notice: 'Post was successfully created.' }
+      format.json { render :show, status: :created, location: @post }
+    else
+      format.html { render :new }
+      format.json { render json: @post.errors, status: :unprocessable_entity }
     end
+  end
+end
+
+def edit
+  @post = Post.find(params[:id])
+end
+
+def update
+  respond_to do |format|
+    if @post.update(post_params)
+      format.html { redirect_to posts_url, notice: 'Post was successfully updated.' }
+      format.json { render :show, status: :ok, location: @post }
+    else
+      format.html { render :edit }
+      format.json { render json: @post.errors, status: :unprocessable_entity }
+    end
+  end
 end
 
 private
